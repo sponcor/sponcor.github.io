7d6154d05d98de076095336b815c3b08f6431fa8
Spencer Taliaferro <spta1738@colorado.edu>
2023-11-07 23:17:56 -0700

    WA10

---
+++ ica/Week11Lecture/week11.js
@@ -0,0 +1,55 @@
+console.log("Making sure this is linked correctly.");
+
+const x = 2;
+const y = 2;
+
+if ( x === y) {
+    console.log("SUCCESS");
+}
+else {
+    console.log("FAIL");
+}
+
+const btn = document.querySelector
+("button");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+document.addEventListener
+("DOMContentLoaded", () => {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+})
+
+btn.addEventListener("click", draw);
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+};
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < 100; i++) {
+        ctx.beginPath();
+        let red = random(255);
+        let green = random(255);
+        let blue = random(255);
+        let alpha = Math.random(); 
+        let color = "rgba(" + red + "," + green + "," + blue + "," + alpha + ")";
+        console.log(color);
+        ctx.fillStyle = color; 
+        ctx.arc(
+            random(canvas.width),
+            random(canvas.height),
+            random(50),
+            0,
+            2 * Math.PI
+        );
+        ctx.fill();
+    }
+}
+
+btn.addEventListener("click", draw);
+
+//change something about draw function
